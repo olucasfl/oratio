b272755ade83368bbdea2552d8cf2f4ede0179e5
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-09 19:23:34 -0300

    feat: implement protect routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,29 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Home from "./pages/Home/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App(){
+function App() {
 
  return(
 
   <Routes>
 
-   <Route path="/" element={<Navigate to="/login"/>} />
+   <Route path="/" element={<Navigate to="/login" replace />} />
 
-   <Route path="/login" element={<Login/>}/>
-   <Route path="/register" element={<Register/>}/>
+   <Route path="/login" element={<Login />} />
+   <Route path="/register" element={<Register />} />
 
-   <Route path="/oratio/home" element={<Home/>}/>
+   <Route
+    path="/oratio/home"
+    element={
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    }
+   />
+
+   <Route path="*" element={<Navigate to="/login" replace />} />
 
   </Routes>
 
