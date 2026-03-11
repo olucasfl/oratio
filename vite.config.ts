4f1d7bd94b2a65caefaa43a797c30ff56ed76e94
Lucas F. Leandro <lucasfariasleandro@gmail.com>
2026-03-10 23:48:51 -0300

    refactor: implement new rules to vite config for optmization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+plugins: [
+react()
+],
+
+build: {
+
+target: "esnext",
+
+sourcemap: false,
+
+minify: "esbuild",
+
+chunkSizeWarningLimit: 1000,
+
+rollupOptions: {
+
+output: {
+
+manualChunks: {
+
+react: ["react","react-dom"],
+
+router: ["react-router-dom"]
+
+}
+
+}
+
+}
+
+},
+
+optimizeDeps: {
+
+include: [
+"react",
+"react-dom",
+"react-router-dom"
+]
+
+},
+
+server: {
+
+port: 5173
+
+}
+
 })
