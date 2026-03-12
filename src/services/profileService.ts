5e464540ffcf3aeb625d5324029d631aad12dc5e
Lucas F. Leandro <lucasfariasleandro@gmail.com>
2026-03-12 11:55:02 -0300

    refactor: profile redirect service correctly

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,20 +1,9 @@
+import api from "./api"
+
 export async function getProfile(){
 
- const token = localStorage.getItem("access_token")
+ const res = await api.get("/users/me")
 
- const res = await fetch(
-  "https://finance-api-y0ol.onrender.com/users/me",
-  {
-   headers:{
-    Authorization:`Bearer ${token}`
-   }
-  }
- )
-
- if(!res.ok){
-  throw new Error("Erro ao carregar perfil")
- }
-
- return res.json()
+ return res.data
 
 }
