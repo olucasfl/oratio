61debd96d031fcab13bd69ebc75e8b4658cacd84
Lucas F. Leandro <lucasfariasleandro@gmail.com>
2026-04-04 14:54:13 -0300

    fix: bug scrolling on modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -99,6 +99,21 @@ export default function Home(){
   return () => window.removeEventListener("keydown", onEsc)
  },[modal, selector])
 
+ useEffect(() => {
+    if (modal || selector) {
+        document.body.style.overflow = "hidden"
+        document.body.style.touchAction = "none"
+    } else {
+        document.body.style.overflow = ""
+        document.body.style.touchAction = ""
+    }
+
+    return () => {
+        document.body.style.overflow = ""
+        document.body.style.touchAction = ""
+    }
+    }, [modal, selector])
+
  function handleLogout(){
   localStorage.removeItem("access_token")
   localStorage.removeItem("refresh_token")
