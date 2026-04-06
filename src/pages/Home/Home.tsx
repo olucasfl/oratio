8a4a07ec0bd90756ccfca55a5f4e9bf22f8eb254
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-04-06 13:50:16 -0300

    fix: scrolling problem on liturgic modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -103,19 +103,43 @@ export default function Home(){
  },[modal, selector])
 
  useEffect(() => {
-    if (modal || selector) {
-        document.body.style.overflow = "hidden"
-        document.body.style.touchAction = "none"
-    } else {
-        document.body.style.overflow = ""
-        document.body.style.touchAction = ""
-    }
+  if (modal || selector) {
 
-    return () => {
-        document.body.style.overflow = ""
-        document.body.style.touchAction = ""
+    const scrollY = window.scrollY
+
+    document.body.style.position = "fixed"
+    document.body.style.top = `-${scrollY}px`
+    document.body.style.left = "0"
+    document.body.style.right = "0"
+    document.body.style.overflow = "hidden"
+    document.body.style.width = "100%"
+
+  } else {
+
+    const scrollY = document.body.style.top
+
+    document.body.style.position = ""
+    document.body.style.top = ""
+    document.body.style.left = ""
+    document.body.style.right = ""
+    document.body.style.overflow = ""
+    document.body.style.width = ""
+
+    if(scrollY){
+      window.scrollTo(0, parseInt(scrollY || "0") * -1)
     }
-    }, [modal, selector])
+  }
+
+  return () => {
+    document.body.style.position = ""
+    document.body.style.top = ""
+    document.body.style.left = ""
+    document.body.style.right = ""
+    document.body.style.overflow = ""
+    document.body.style.width = ""
+  }
+
+}, [modal, selector])
 
  function handleLogout(){
   localStorage.removeItem("access_token")
