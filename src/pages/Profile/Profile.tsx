c19707ee796a9b08007a9e1102de8b8aecb6ff64
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-06-01 16:58:55 -0300

    feat: implement new option to search on bible, new feature to confissão

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { useEffect,useState,useMemo } from "react"
+import { useEffect,useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useOffline } from "../../hooks/useOffline"
 
